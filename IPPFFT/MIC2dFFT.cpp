4a58d1d5ae7c09c5da5cfa66a028790a8feccfaf
Zihao Wang <652611733@qq.com>
2017-02-23 22:21:48 +0800

    Update MIC2dFFT.cpp
    Right MIC version using compile command :
    icc main.cpp -o Application  -qoffload-option,mic,link,"-L${IPPROOT}/lib/mic -lippi -lipps -lippcore -lippvm"

--- IPPFFT/MIC2dFFT.cpp
+++ IPPFFT/MIC2dFFT.cpp
@@ -1,3 +1,4 @@
+
 #include "ipp.h"
 #include "ippi.h"
 #include <stdio.h>
@@ -5,61 +6,58 @@
 #include<string.h>
 #include<sys/time.h>
 
-
 int main(int argc, char* argv[]) {
 
-	IppStatus status;
 	int N[2];
-	N[0] = 512;
-	N[1] = 512;
+	N[0] = 4096 * 2;
+	N[1] = 4096 * 2;
 	Ipp32fc *in, *out;
 	in = (Ipp32fc *) malloc(N[0] * N[1] * sizeof(Ipp32fc));
-	out = (Ipp32fc *) malloc(N[0] * N[1] * sizeof(Ipp32fc));	
-/*for (int x = 0; x < N[0] * N[1]; x++) {
+	out = (Ipp32fc *) malloc(N[0] * N[1] * sizeof(Ipp32fc));
+	for (int x = 0; x < N[0] * N[1]; x++) {
 		in[x].re = x % 1000;
 		in[x].im = 0;
 	}
-*/
 #pragma offload target(mic:0) inout(out:length(N[0]*N[1])) \
-                                      in(in:length(N[0]*N[1]))
-
+        								in(in:length(N[0]*N[1]))
 	{
+
 		struct timeval tv1, tv2;
 		struct timezone tz;
 		ippInit();
-		gettimeofday(&tv1, &tz);
-		for (int x = 0; x < N[0] * N[1]; x++) {
-			in[x].re = x % 1000;
-			in[x].im = 0;
-		}
-		IppiFFTSpec_C_32fc *pSpec = NULL; /* Pointer to FFT spec structure */
-		Ipp8u *pMemInit = NULL, *pBuffer = NULL; /* Pointer to the work buffers */
-		int sizeSpec = 0, sizeInit = 0, sizeBuf = 0; /* Size of FFT spec structure, init and work buffers */
+		Ipp8u *pMemInit = NULL, *pBuffer = NULL;
+		int sizeSpec = 0, sizeInit = 0, sizeBuf = 0;
+		IppiFFTSpec_C_32fc *pSpec = NULL;
+		//the order is important for GetSize and Init_C
+		ippiFFTGetSize_C_32fc(13, 13, IPP_FFT_DIV_INV_BY_N, ippAlgHintAccurate,
+				&sizeSpec, &sizeInit, &sizeBuf);
+
 		pSpec = (IppiFFTSpec_C_32fc*) ippMalloc(sizeSpec);
 		pBuffer = (Ipp8u*) ippMalloc(sizeBuf);
 		pMemInit = (Ipp8u*) ippMalloc(sizeInit);
 
-		ippiFFTInit_C_32fc(N[0], N[1], IPP_FFT_DIV_INV_BY_N, ippAlgHintAccurate,
+		ippiFFTInit_C_32fc(13, 13, IPP_FFT_DIV_INV_BY_N, ippAlgHintAccurate,
 				pSpec, pMemInit);
-		ippiFFTGetSize_C_32fc(N[0], N[1], IPP_FFT_DIV_INV_BY_N,
-				ippAlgHintAccurate, &sizeSpec, &sizeInit, &sizeBuf);
-		for (int x = 0; x < 100; x++) {
-			ippiFFTFwd_CToC_32fc_C1R(in, 8 * sizeof(Ipp32fc), out,
-					8 * sizeof(Ipp32fc), pSpec, pBuffer);
 
+		gettimeofday(&tv1, &tz);
+		for (int i = 0; i < 100; i++) {
+			ippiFFTFwd_CToC_32fc_C1R(in, N[0] * sizeof(Ipp32fc), out,
+					N[0] * sizeof(Ipp32fc), pSpec, pBuffer);
 		}
 		gettimeofday(&tv2, &tz);
-
 		double timeuse = tv2.tv_sec - tv1.tv_sec
 				+ (tv2.tv_usec - tv1.tv_usec) / 1000000.0;
 		printf(" exact time    FFT  tv_sec:  %f s \n", timeuse);
 		printf(" pureFFT   FFT  tv_sec:  %f s \n", tv2.tv_sec - tv1.tv_sec);
+
+		for (int i = 0; i < 10; i++)
+			printf("%f %f\n", out[i].re, out[i].im);
+		printf("\n");
+
 		ippFree(pMemInit);
 		ippFree(pSpec);
 		ippFree(pBuffer);
 	}
-	for (int i = 0; i < 10; i++)
-		printf("%f %f\n", out[i].re, out[i].im);
-	printf("\n");
+	return 0;
 
 }
